0a9e77ff0b07f42423f8c4dbba3d3c71924dc8c9
changsheng <changsheng@DESKTOP-OITVNEC>
2023-08-04 09:51:01 +0800

    添加项目文件。

---
+++ KeyboardLock/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace KeyboardLock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Notify_Caps = new System.Windows.Forms.NotifyIcon(this.components);
+            this.Notify_Num = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.显示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Notify_Caps
+            // 
+            this.Notify_Caps.ContextMenuStrip = this.contextMenuStrip1;
+            this.Notify_Caps.Icon = ((System.Drawing.Icon)(resources.GetObject("Notify_Caps.Icon")));
+            this.Notify_Caps.Visible = true;
+            // 
+            // Notify_Num
+            // 
+            this.Notify_Num.ContextMenuStrip = this.contextMenuStrip1;
+            this.Notify_Num.Icon = ((System.Drawing.Icon)(resources.GetObject("Notify_Num.Icon")));
+            this.Notify_Num.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.显示ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // 显示ToolStripMenuItem
+            // 
+            this.显示ToolStripMenuItem.Name = "显示ToolStripMenuItem";
+            this.显示ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.显示ToolStripMenuItem.Text = "显示";
+            this.显示ToolStripMenuItem.Click += new System.EventHandler(this.显示ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(144, 81);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon Notify_Caps;
+        private System.Windows.Forms.NotifyIcon Notify_Num;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 显示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+    }
+}
+
---
+++ KeyboardLock/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace KeyboardLock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll", EntryPoint = "GetKeyboardState")]
+
+        public static extern int GetKeyboardState(byte[] pbKeyState);
+
+        [DllImport("user32.dll")]
+
+        public static extern short GetAsyncKeyState(int key);
+
+        [DllImport("user32.dll", EntryPoint = "GetKeyState")]
+
+        public static extern short GetKeyState(int key);
+
+        public static bool CapsLockStatus
+        {
+            get
+            {
+                byte[] bs = new byte[256];
+                GetKeyboardState(bs);
+                return (bs[0x14] == 1);
+            }
+        }
+
+        public static bool NumLockStatus
+        {
+            get
+            {
+                byte[] bs = new byte[256];
+                GetKeyboardState(bs);
+                return (bs[0x90] == 1);
+            }
+        }
+
+        bool caps = false;
+        bool num = false;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //if (GetAsyncKeyState(20) == -32768)
+            //{ caps = true; }
+            //else { caps = false; }
+
+            //if (GetAsyncKeyState(144) == -32768)
+            //{ num = true; }
+            //else { num = false; }
+
+            short capsState = GetKeyState(20);
+            short numState = GetKeyState(144);
+
+            if (capsState == 1 || capsState==-127)
+            { caps = true; }
+            else { caps = false; }
+
+            if (numState == 1 || capsState == -127)
+            { num = true; }
+            else { num = false; }
+
+
+            if (caps)
+            { Notify_Caps.Icon = new Icon("icons/CapsLock.ico"); }
+            else
+            { Notify_Caps.Icon = new Icon("icons/CapsUnlock.ico"); }
+
+
+            if (num)
+                Notify_Num.Icon = new Icon("icons/NumLock.ico");
+            else
+                Notify_Num.Icon = new Icon("icons/NumUnlock.ico");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //this.WindowState = FormWindowState.Minimized;
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                //是否取消close操作
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.WindowState = FormWindowState.Minimized;
+            //this.Hide();
+
+            if (CapsLockStatus)
+            { caps = true; }
+            else
+            { caps = false; }
+
+
+            if (NumLockStatus)
+            { num = true; }
+            else
+            { num = false; }
+
+            //this.Hide();
+        }
+
+        private void 显示ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
